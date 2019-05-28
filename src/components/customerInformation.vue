8c537e542dce5354ede5ea727b6f86e8e39aa733
varangian111 <rrowe222@gmail.com>
2019-05-28 15:13:20 -0500

    added line breaks to templates so it's easier to parse for now

--- src/components/customerInformation.vue
+++ src/components/customerInformation.vue
@@ -1,9 +1,13 @@
 <template>
 	<form id="customer-information" class="bg-blue-400 py-4 px-4">
-		<input v-model="name" type="text" id="customer-name" placeholder="Customer Name" class="px-4 py-2">
-		<input v-model="phone" type="tel" id="Phone Number" placeholder="Phone Number" class="px-4 py-2">
-		<input v-model="address" type="text" id="customer-address" placeholder="Address" class=" px-4 py-2">
-		<input v-model="account" type="number" id="customer-account" placeholder="Account #" class=" px-4 py-2">
+		<input v-model="name" type="text" id="customer-name" placeholder="Customer Name" class="px-4 py-2 m-2">
+		<br>
+		<input v-model="phone" type="tel" id="Phone Number" placeholder="Phone Number" class="px-4 py-2 m-2">
+		<br>
+		<input v-model="address" type="text" id="customer-address" placeholder="Address" class=" px-4 py-2 m-2">
+		<br>
+		<input v-model="account" type="number" id="customer-account" placeholder="Account #" class=" px-4 py-2 m-2">
+		<br>
 	</form>
 </template>
 
--- src/components/returnInformation.vue
+++ src/components/returnInformation.vue
@@ -1,12 +1,17 @@
 <template>
-	<form>
-		<input type="radio" id="Swap" value="Swap" v-model="picked">
+	<div class="mx-auto">
+	<form class="text-left">
+		<input name="equipment" type="radio" id="Swap" value="Swap" v-model="picked">
 		<label for="Swap">Swapping Equipment</label>
-		<input type="radio" id="Cancelling" value="Cancelling" v-model="picked">
+		<br>
+		<input name="equipment" type="radio" id="Cancelling" value="Cancelling" v-model="picked">
 		<label for="Cancelling">Cancelling Service</label>
-		<input type="radio" id="Moving" value="Moving" v-model="picked">
+		<br>
+		<input name="equipment" type="radio" id="Moving" value="Moving" v-model="picked">
 		<label for="Moving">Moving</label>
+		<br>
 	</form>
+	</div>
 </template>
 
 <script>
