7f571a47535f7136452060cc2f4fea086933492d
varangian111 <rrowe222@gmail.com>
2019-05-31 16:37:03 -0500

    Took colin's advice and now it's even more broken

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,47 @@
 <template>
-  <div id="app">
-    <h1 class="text-2xl">HBC Return Exchange Form</h1>
-    <customerInformation />
-    <equipment-input />
-    <return-information />
-    <notes />
-    <button @click="exportPDF">Export</button>
-  </div>
+	<div id="app" @customer-data-entered="test">
+		<h1 class="text-2xl">HBC Return Exchange Form</h1>
+		<customerInformation />
+		<equipment-input />
+		<return-information />
+		<notes />
+		<button @click="exportPDF">Export</button>
+	</div>
 </template>
 
 <script>
-  import customerInformation from './components/customerInformation'
-  import EquipmentInput from "./components/equipmentInput";
-  import Notes from "./components/notes";
-  import ReturnInformation from "./components/returnInformation";
-  import jsPDF from 'jspdf'
+	import customerInformation from './components/customerInformation'
+	import EquipmentInput from "./components/equipmentInput";
+	import Notes from "./components/notes";
+	import ReturnInformation from "./components/returnInformation";
+	import jsPDF from 'jspdf'
 
 
-  export default {
-  name: 'app',
-  components: {
-    ReturnInformation,
-    Notes,
-    EquipmentInput,
-    customerInformation,
-  },
-  methods:{
-    exportPDF(){
-    	let test = customerInformation.customerData();
-    	console.log(test);
-      //let doc = new jsPDF('p', `pt`);
-      //doc.save("return.pdf");
-    }
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			ReturnInformation,
+			Notes,
+			EquipmentInput,
+			customerInformation,
+		},
+		methods:{
+			exportPDF(){
+				console.log(customerInformation.data());
+				//let doc = new jsPDF('p', `pt`);
+				//doc.save("return.pdf");
+			}
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
--- src/components/customerInformation.vue
+++ src/components/customerInformation.vue
@@ -1,19 +1,18 @@
 <template>
-	<form @submit.prevent="onSubmit" v-model="customerInformation" id="customer-information" class="bg-blue-400 py-4 px-4">
-		<input type="text" v-model="customerName" id="customerName" placeholder="Customer Name" class="px-4 py-2 m-2">
+	<form @submit.prevent="onSubmit" id="customer-information" class="bg-blue-400 py-4 px-4">
+		<input type="text" v-model="form.customerName" id="customerName" placeholder="Customer Name" class="px-4 py-2 m-2">
 		<br>
-		<input type="tel" v-model.number="customerPhoneNumber" id="Phone Number" placeholder="Phone Number" class="px-4 py-2 m-2">
+		<input type="tel" v-model.number="form.customerPhoneNumber" id="Phone Number" placeholder="Phone Number" class="px-4 py-2 m-2">
 		<br>
-		<input type="text" v-model="customerAddress" id="customer-address" placeholder="Address" class=" px-4 py-2 m-2">
+		<input type="text" v-model="form.customerAddress" id="customer-address" placeholder="Address" class=" px-4 py-2 m-2">
 		<br>
-		<input type="number" v-model.number="customerAccount" id="customer-account" placeholder="Account #" class=" px-4 py-2 m-2">
+		<input type="number" v-model.number="form.customerAccount" id="customer-account" placeholder="Account #" class=" px-4 py-2 m-2">
 		<br>
 	</form>
 </template>
 
 <script>
 	export default {
-		name: 'customerName',
 		data(){
 			return{
 					customerName: null,
@@ -29,7 +28,7 @@
 					phone : this.customerPhoneNumber,
 					address : this.customerAddress,
 					account : this.customerAccount
-				}
+				};
 				this.$emit('customer-data-entered', customerData);
 			}
 		}
