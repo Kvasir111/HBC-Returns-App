eb1283009ceb4e941628fadf3cabb69cc3d924e1
varangian111 <rrowe222@gmail.com>
2019-06-01 19:21:01 -0500

    Goddamned Finally

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,38 @@
 		<img v-bind:src="image" v-bind:alt="logoAlt">
 		<form id="information input" @submit.prevent="exportPDF" class="mx-auto" v-model="shit">
 			<div id="customerInformation" class="mx-auto">
-			<input type="text" v-model="customerName" placeholder="Customer Name">
-				<input type="number" v-model="customerAccount" placeholder="Account Number">
-				<input type="tel" v-model="customerPhoneNumber" placeholder="Phone number">
-				<input type="text" v-model="customerAddress" placeholder="Service Address">
+			<input type="text" v-model="customerName" placeholder="Customer Name" v-bind:aria-valuemax="customerName">
+				<input type="number" v-model="customerAccount" placeholder="Account Number" v-bind:aria-valuemax="customerAccount">
+				<input type="tel" v-model="customerPhoneNumber" placeholder="Phone number" v-bind:aria-valuemax="customerPhoneNumber">
+				<input type="text" v-model="customerAddress" placeholder="Service Address" v-bind:aria-valuemax="customerAddress">
 			</div>
 			<div id="equipment-type">
-				<select v-model="equipmentType" class="p-2 m-2">
+				<select v-model="equipmentType" class="p-2 m-2" v-bind:aria-valuemax="equipmentType">
 					<option disabled >Select Equipment Type</option>
 					<option value="DVR/Set-Top Box">DVR/Set-Top Box</option>
 					<option value="Gateway/Modem">Gateway/Modem</option>
 					<option value="Wireless Router">Wireless Router</option>
 					<option value="DTA">DTA</option>
 				</select>
-				<input class="p-2 m-2" v-model="equipmentNum" type="text" placeholder="CMAC/SN">
-				<input class="p-2 m-2" type="checkbox" v-model="powerCord" true-value="Power Cord" false-value="">Power Cord?
-				<input class="p-2 m-2" type="checkbox" v-model="remote" true-value="Remote" false-value="">Remote?
+				<input class="p-2 m-2" v-model="equipmentNum" type="text" placeholder="CMAC/SN" v-bind:aria-valuemax="equipmentNum">
+				<input class="p-2 m-2" type="checkbox" v-model="powerCord" true-value="Power Cord" false-value="" v-bind:aria-valuemax="powerCord">Power Cord?
+				<input class="p-2 m-2" type="checkbox" v-model="remote" true-value="Remote" false-value="" v-bind:aria-valuemax="remote">Remote?
 			</div>
 			<div id="return-information">
-				<input name="equipment" type="radio" id="Swap" value="Swap" v-model="returnType">
+				<input name="equipment" type="radio" id="Swap" value="Swap" v-model="returnType" v-bind:aria-valuemax="returnType">
 				<label for="Swap">Swapping Equipment</label>
 				<br>
-				<input name="equipment" type="radio" id="Cancelling" value="Cancelling" v-model="returnType">
+				<input name="equipment" type="radio" id="Cancelling" value="Cancelling" v-model="returnType" v-bind:aria-valuemax="returnType">
 				<label for="Cancelling">Cancelling Service</label>
 				<br>
-				<input name="equipment" type="radio" id="Moving" value="Moving" v-model="returnType">
+				<input name="equipment" type="radio" id="Moving" value="Moving" v-model="returnType" v-bind:aria-valuemax="returnType">
 				<label for="Moving">Moving</label>
 				<br>
 			</div>
 			<div id="notes">
 				<span>Notes</span>
 				<br>
-				<textarea v-model="notes" placeholder="Enter notes here"></textarea>
+				<textarea v-model="notes" placeholder="Enter notes here" v-bind:aria-valuemax="notes"></textarea>
 			</div>
 			<input type="submit" value="Submit">
 		</form>
@@ -43,6 +43,7 @@
 
 <script>
 	import jspdf from 'jspdf'
+
 	export default {
 		component:{
 		},
@@ -78,7 +79,8 @@
 				let account = this.customerAccount;
 				let returnType = this.returnType;
 				let notes = this.notes;
-				let data = [customerName, phone, address, account, returnType, notes];
+				let k = this.shit;
+				let data = [name, phone, address, account, returnType, notes];
 			let doc = new jspdf({
 				orientation: 'p',
 				unit:        'in',
@@ -86,8 +88,8 @@
 				
 			});
 			console.log(data)
-			//doc.text(data, 2, 2);
-			//doc.save("return.pdf")
+			doc.text(data, 2, 2);
+			doc.save("return.pdf")
 			
 			},
 
