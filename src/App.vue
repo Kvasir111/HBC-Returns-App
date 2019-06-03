b90db08ff642afb16563ca77264355cd8e470fb9
varangian111 <rrowe222@gmail.com>
2019-06-02 20:51:04 -0500

    split input columnsv and restyled inputs to be less shitty looking

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,40 @@
 <template>
 	<div id="app" class="">
 		<img v-bind:src="image" v-bind:alt="logoAlt" class="mx-auto " width="200px">
-		<h1 class="text-center text-2xl font-bold p-2">Enter Information for Equipment Return</h1>
+		<h1 class="text-center text-2xl font-bold text-white p-2">Enter Information for Equipment Return</h1>
 		<form id="information input" @submit.prevent="exportPDF" class="bg-white shadow-md w-2/3 mx-auto p-2">
 			<div id="customerInformation" class="p-2">
+				<div class="text-center">
 				<input type="text" autocomplete="no" required v-model="customerName" placeholder="Customer Name"
-				       v-bind:aria-valuemax="customerName" class="cardInput mx-auto">
+				       v-bind:aria-valuemax="customerName" class="form-input m-2">
 				<input type="number" min="0" v-model="customerAccount" placeholder="Account Number"
-				       v-bind:aria-valuemax="customerAccount" class="cardInput mx-auto">
+				       v-bind:aria-valuemax="customerAccount" class="form-input m-2">
+				</div>
+				<div class="text-center">
 				<input type="tel" required minlength="10" maxlength="11" v-model="customerPhoneNumber" placeholder="Phone number"
-				       v-bind:aria-valuemax="customerPhoneNumber" class="cardInput mx-auto">
+				       v-bind:aria-valuemax="customerPhoneNumber" class="form-input m-2">
 				<input type="text" required v-model="customerAddress" placeholder="Service Address"
-				       v-bind:aria-valuemax="customerAddress" class="cardInput mx-auto">
-			</div>
+				       v-bind:aria-valuemax="customerAddress" class="form-input m-2">
+				</div>
+				</div>
 			<div id="equipment-type" class="text-center">
-				<select v-model="equipmentType" class="p-2 m-2 bg-gray-200" v-bind:aria-valuemax="equipmentType">
-					<option selected disabled class="text-black">Select Equipment Type</option>
+				<label for="equipmentDropdown" invisible></label>
+				<select v-model="equipmentType" id="equipmentDropdown" class="form-multiselect bg-gray-200 m-2" v-bind:aria-valuemax="equipmentType">
+					<option selected disabled class="">Select Equipment Type</option>
 					<option value="DVR/Set-Top Box">DVR/Set-Top Box</option>
 					<option value="Gateway/Modem">Gateway/Modem</option>
 					<option value="Wireless Router">Wireless Router</option>
 					<option value="DTA">DTA</option>
 				</select>
-				<input class="equipmentInput" v-model="equipmentNum" type="text" placeholder="CMAC/SN"
+				<input class="form-input inline" v-model="equipmentNum" type="text" placeholder="CMAC/SN"
 				       v-bind:aria-valuemax="equipmentNum">
-				<input class="p-2 m-2" type="checkbox" v-model="powerCord" true-value="Power Cord" false-value=""
+				<input class="p-2 m-2 form-checkbox" type="checkbox" v-model="powerCord" true-value="Power Cord" false-value=""
 				       v-bind:aria-valuemax="powerCord">Power Cord?
-				<input class="p-2 m-2" type="checkbox" v-model="remote" true-value="Remote" false-value=""
+				<input class="p-2 m-2 form-checkbox" type="checkbox" v-model="remote" true-value="Remote" false-value=""
 				       v-bind:aria-valuemax="remote">Remote?
 			</div>
 			<div id="return-information" class="text-center">
-				<select v-model="returnType" class="py-2 m-2 bg-gray-200">
+				<select v-model="returnType" class="form-multiselect m-2 bg-gray-200">
 					<option selected disabled>Select a reason for return</option>
 					<option value="Exchange/Defective Equipment">Exchange/Defective Equipment</option>
 					<option value="Cancelling Service">Cancelling Service</option>
@@ -40,7 +45,7 @@
 			<div id="notes" class="mx-auto block">
 				<textarea rows="4" cols="50" placeholder="Enter notes here" v-model="notes" class="mx-auto block bg-gray-200 p-2"></textarea>
 			</div>
-			<input type="submit" value="Submit" class="mx-auto block px-4 py-2 m-2 rounded hover:bg-blue-300">
+			<input type="submit" value="Submit" class="mx-auto newInput">
 		</form>
 	</div>
 </template>
@@ -114,9 +119,7 @@
 				});
 				doc.text(data, 2, 2);
 				doc.save("return.pdf")
-
 			},
-
 		}
 	}
 </script>
