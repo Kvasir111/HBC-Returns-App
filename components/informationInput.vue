234905f2b1fc6feec730b95e1c157757e1882eff
varangian111 <rrowe222@gmail.com>
2019-07-02 10:50:30 -0500

    Removed spinner from account number

--- components/informationInput.vue
+++ components/informationInput.vue
@@ -223,5 +223,9 @@
 </script>
 
 <style scoped>
-
+	input[type=number]::-webkit-inner-spin-button,
+	input[type=number]::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
 </style>
